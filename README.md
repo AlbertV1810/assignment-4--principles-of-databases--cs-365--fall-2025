61fa029ec095040acae448c3b94074da3a1adae5
Roy A Vanegas <roy@thecodeeducators.com>
2025-11-19 09:38:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Fall 2025 Principles of Databases — Assignment 4
+
+* **Read these instructions repeatedly until you understand, then begin your project. If something is not clear, ask.**
+
+## ❖ Before You Begin ❖
+
+1. Log in to GitHub.
+2. Fork this repo(sitory). See [this video](http://code-warrior.github.io/tutorials/git/github/forking-and-cloning-at-the-github-web-site/) on how to carry out this step and step `3`.
+3. Clone your fork, using either the web site or the GitHub Desktop client.
+4. Checkout your personalized branch, the one with your name and GitHub handle.
+
+---
+
+## ❖・Introduction・❖
+
+For this assignment, you will complete update and delete routines in a web app using the MENN (Mongo, Express, Nunjucks, Node) stack. You will not need to add any files to this scaffold; all required files are already included.
+
+Routines — or routes — for the update and delete procedures must go in the included `server.js` file. When either route is carried out successfully, a message in the CLI should report that the task was carried out. For example, if a record was removed, your program should report which record was removed. Do the same for the update route. A starter database is included in this project (see below).
+
+---
+
+## ❖・Stand Up the MENN (Mongo, Express, Nunjucks, Node) Stack・❖
+
+1. Start the Mongo server.
+2. Open a CLI window and log in to Mongo: `mongosh`
+3. Open another CLI window and navigate to the `models` folder.
+4. Run `mongorestore`, which will restore the `project` database containing the `users` collection from the `dump` folder.
+5. Navigate to the root folder of this repo.
+6. Install the dependencies from the `package.json` file by running `npm i`.
+7. Run `npm run dev`.
+8. Visit `http://localhost:3000` in any browser.
+
+---
+
+## ❖・Grading・❖
+
+| Item                                   | Points |
+|----------------------------------------|:------:|
+| Following directions                   |  `25`  |
+| Syntax, style, and neatness            |  `25`  |
+| Update router carried out successfully |  `25`  |
+| Delete router carried out successfully |  `25`  |
+
+---
+
+## ❖・Due・❖
+
+Wednesday, 10 December 2025, at 2:00 PM. ***Note*: Per the syllabus, NO late submissions will be accepted**
+
+---
+
+## ❖・Submission・❖
+
+You will need to issue a pull request back into the original repo, the one from which your fork was created for this project. See the **Issuing Pull Requests** section of [this site](http://code-warrior.github.io/tutorials/git/github/index.html) for help on how to submit your assignment.
+
+**Note**: This assignment may *only* be submitted via GitHub. **No other form of submission will be accepted**.
---
+++ server.js
@@ -0,0 +1,173 @@
+const express = require(`express`);
+const app = express();
+const nunjucks = require(`nunjucks`);
+const bodyParser = require(`body-parser`);
+const mongoDB = require(`mongodb-legacy`);
+const mongoClient = mongoDB.MongoClient;
+const HOST = `localhost`;
+const dbPort = `27017`;
+const dbURL = `mongodb://${HOST}`;
+const dbName = `project`;
+const dbCollection = `users`;
+const PORT = 3000;
+const port = (process.env.PORT || PORT);
+const colors = {
+    reset: `\x1b[0m`,
+    red: `\x1b[31m`,
+    green: `\x1b[32m`,
+    yellow: `\x1b[33m`,
+};
+
+let db;
+
+/*
+ * Configure the “views” folder to work with Nunjucks
+ */
+nunjucks.configure(`views`, {
+    express: app,
+    autoescape: true
+});
+
+/*
+ * Configure the Node MongoDB client to connect to Mongo, established a database
+ * connection and assigning the reference to the “db” variable defined on line
+ * 21
+ */
+mongoClient.connect(`${dbURL}:${dbPort}`, (err, client) => {
+    if (err) {
+        return console.log(err);
+    } else {
+        db = client.db(dbName);
+
+        console.log(`MongoDB successfully connected:`);
+        console.log(`\tMongo URL:`, colors.green, dbURL, colors.reset);
+        console.log(`\tMongo port:`, colors.green, dbPort, colors.reset);
+        console.log(`\tMongo database name:`,
+            colors.green, dbName, colors.reset, `\n`);
+    }
+});
+
+/*
+ * Configure Node to act as a web server
+ */
+app.listen(port, HOST, () => {
+    console.log(`Host successfully connected:`);
+    console.log(`\tServer URL:`, colors.green, `localhost`, colors.reset);
+    console.log(`\tServer port:`, colors.green, port, colors.reset);
+    console.log(`\tVisit http://localhost:${port}\n`);
+});
+
+/*
+ * Express’s way of setting a variable. In this case, set the variable “view
+ * engine” to “njk” for Nunjucks
+ */
+app.set(`view engine`, `njk`);
+
+/*
+ * Express’s middleware to parse incoming, form-based request data before
+ * processing form data
+ */
+app.use(bodyParser.urlencoded({extended: true}));
+
+/*
+ * Express’s middleware to parse incoming request bodies before handlers
+ */
+app.use(bodyParser.json());
+
+/*
+ * Express’s middleware to serve HTML, CSS, and JavaScript files from the
+ * included “public” folder. Note: There are no JavaScript files in the
+ * “public” folder
+ */
+app.use(express.static(`public`));
+
+/*
+ * Note:
+ *   — “req” stands for requests, which arrive from the client/browser
+ *   — “res” stands for responses, which are sent to the client/browser
+ */
+
+/*
+ * This router handles GET requests to the root of the website
+ */
+app.get(`/`, (req, res) => {
+    console.log(`User requested root of web site.`);
+    console.log(`Responding to request with file`,
+        colors.green, `index.njk`, colors.reset, `via GET.`);
+
+    res.render(`index.njk`);
+});
+
+/*
+ * This router handles GET requests to http://localhost:3000/read-a-db-record/
+ */
+app.get(`/read-a-db-record`, (req, res) => {
+    db.collection(dbCollection).find().toArray((err, arrayObject) => {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log(`User requested http://${HOST}:${port}/read-a-db-record.`);
+            console.log(`Responding to request with file`,
+                colors.green, `read-from-database.njk`, colors.reset, `via GET.\n`);
+
+            res.render(`read-from-database.njk`, {mongoDBArray: arrayObject});
+        }
+    });
+});
+
+/*
+ * This router handles GET requests to
+ * http://localhost:3000/create-a-db-record/
+ */
+app.get(`/create-a-db-record`, (req, res) => {
+    res.render(`create-a-record-in-database.njk`);
+});
+
+/*
+ * This router handles POST requests — via the Nunjucks partial
+ * “create-a-record-in-database.njk” — submitted from the form located at
+ * http://localhost:3000/create-a-db-record/
+ */
+app.post(`/create-a-db-record`, (req, res) => {
+    db.collection(dbCollection).insertOne(req.body, (err) => {
+        console.log(req.body);
+
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log(
+                `Inserted one record into Mongo via an HTML form using POST.\n`);
+
+            res.redirect(`/read-a-db-record`);
+        }
+    });
+});
+
+/*
+ * This router handles GET requests to
+ * http://localhost:3000/update-a-db-record/
+ */
+app.get(`/update-a-db-record`, (req, res) => {
+    db.collection(dbCollection).find().toArray((err, arrayObject) => {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log(`User requested the resource ` +
+                `http://${HOST}:${port}/update-a-db-record`);
+
+            res.render(`update-a-record-in-database.njk`,
+                {mongoDBArray: arrayObject});
+        }
+    });
+});
+
+/*
+ * This router handles GET requests to
+ * http://localhost:3000/delete-a-db-record/
+ */
+app.get(`/delete-a-db-record`, (req, res) => {
+    db.collection(dbCollection).find().toArray((err, arrayObject) => {
+        res.render(`delete-a-record-in-database.njk`,
+            {mongoDBArray: arrayObject});
+    });
+});
