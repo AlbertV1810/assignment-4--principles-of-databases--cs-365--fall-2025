e8ea8cd484c420f5d4382f47dc20f7b5a972139a
AlbertV1810 <143655634+AlbertV1810@users.noreply.github.com>
2025-12-16 13:18:26 -0500

    update comment

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ mongoClient.connect(`${dbURL}:${dbPort}`, (err, client) => {
             }
       });
 
-
+      //UPDATE
       app.put('/users/:id', async (req, res) => {
         const id = req.params.id;
         const updatedData = req.body;
